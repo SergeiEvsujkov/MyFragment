e58c3a56d220ffba5e75c8ab035d913937372f23
coffeemolkin <evsujkov@yandex.ru>
2021-04-13 23:09:42 +0700

    Homework 6.1

--- app/src/main/java/com/example/myfragment/NotesBodyFragment.java
+++ app/src/main/java/com/example/myfragment/NotesBodyFragment.java
@@ -1,30 +1,25 @@
 package com.example.myfragment;
 
-import android.content.res.TypedArray;
+
 import android.os.Bundle;
-
-import androidx.appcompat.widget.AppCompatImageView;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import ru.geekbrains.cityheraldry.event_bus.EventBus;
-import ru.geekbrains.cityheraldry.event_bus.events.ButtonClickedEvent;
+import androidx.fragment.app.Fragment;
+
+import com.example.myfragment.event_bus.EventBus;
+
+import com.example.myfragment.event_bus.events.ButtonClickedEvent;
 
 public class NotesBodyFragment extends Fragment {
 
     public static final String ARG_NOTE = "note";
     private Note note;
 
-    // Фабричный метод создания фрагмента
-    // Фрагменты рекомендуется создавать через фабричные методы.
     public static NotesBodyFragment newInstance(Note note) {
         NotesBodyFragment f = new NotesBodyFragment();    // создание
-
-        // Передача параметра
         Bundle args = new Bundle();
         args.putParcelable(ARG_NOTE, note);
         f.setArguments(args);
@@ -42,23 +37,14 @@ public class NotesBodyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Таким способом можно получить головной элемент из макета
         View view = inflater.inflate(R.layout.fragment_notes_body, container, false);
-        // найти в контейнере элемент-изображение
-        AppCompatImageView imageBody = view.findViewById(R.id.body);
-        // Получить из ресурсов массив указателей на изображения гербов
-        TypedArray images = getResources().obtainTypedArray(R.array.body_imgs);
-        // Выбрать по индексу подходящий
-        imageBody.setImageResource(images.getResourceId(note.getImageIndex(), -1));
-        // Установить название города
         TextView noteNameView = view.findViewById(R.id.textView);
+        TextView noteBodyView = view.findViewById(R.id.textBody);
+        TextView noteDateView = view.findViewById(R.id.textDate);
         noteNameView.setText(note.getNoteName());
-        noteNameView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EventBus.getBus().post(new ButtonClickedEvent(5));
-            }
-        });
+        noteBodyView.setText(note.getNoteText());
+        noteDateView.setText(note.getNoteDate());
+        noteNameView.setOnClickListener(v -> EventBus.getBus().post(new ButtonClickedEvent(5)));
         return view;
     }
 }
--- app/src/main/java/com/example/myfragment/NotesFragment.java
+++ app/src/main/java/com/example/myfragment/NotesFragment.java
@@ -18,8 +18,8 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.squareup.otto.Subscribe;
 
-import ru.geekbrains.cityheraldry.event_bus.EventBus;
-import ru.geekbrains.cityheraldry.event_bus.events.ButtonClickedEvent;
+import com.example.myfragment.event_bus.EventBus;
+import com.example.myfragment.event_bus.events.ButtonClickedEvent;
 
 public class NotesFragment extends Fragment {
 
@@ -27,14 +27,12 @@ public class NotesFragment extends Fragment {
     private Note currentNote;
     private boolean isLandscape;
 
-    // При создании фрагмента укажем его макет
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_notes, container, false);
     }
 
-    // вызывается после создания макета фрагмента, здесь мы проинициализируем список
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -53,55 +51,52 @@ public class NotesFragment extends Fragment {
         super.onStop();
     }
 
-    // создаем список городов на экране из массива в ресурсах
+
     private void initList(View view) {
         LinearLayout layoutView = (LinearLayout) view;
         String[] notes = getResources().getStringArray(R.array.notes);
 
-        // В этом цикле создаем элемент TextView,
-        // заполняем его значениями,
-        // и добавляем на экран.
-        // Кроме того, создаем обработку касания на элемент
+
         for (int i = 0; i < notes.length; i++) {
             String note = notes[i];
             TextView tv = new TextView(getContext());
+
             tv.setText(note);
             tv.setTextSize(30);
             layoutView.addView(tv);
+            tv.setTextColor(getResources().getColor(R.color.blue));
             final int fi = i;
             tv.setOnClickListener(v -> {
-                currentNote = new Note(fi, getResources().getStringArray(R.array.notes)[fi]);
+                currentNote = new Note(fi, getResources().getStringArray(R.array.notes)[fi], getResources().getStringArray(R.array.notesBody)[fi], getResources().getStringArray(R.array.notesDate)[fi]);
                 showBody(currentNote);
             });
         }
     }
 
-    // Сохраним текущую позицию (вызывается перед выходом из фрагмента)
+
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         outState.putParcelable(CURRENT_NOTE, currentNote);
         super.onSaveInstanceState(outState);
     }
 
-    // activity создана, можно к ней обращаться. Выполним начальные действия
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        // Определение, можно ли будет расположить рядом герб в другом фрагменте
+
         isLandscape = getResources().getConfiguration().orientation
                 == Configuration.ORIENTATION_LANDSCAPE;
 
-        // Если это не первое создание, то восстановим текущую позицию
+
         if (savedInstanceState != null) {
-            // Восстановление текущей позиции.
             currentNote = savedInstanceState.getParcelable(CURRENT_NOTE);
         } else {
-            // Если воccтановить не удалось, то сделаем объект с первым индексом
-            currentNote = new Note(0, getResources().getStringArray(R.array.notes)[0]);
+            currentNote = new Note(0, getResources().getStringArray(R.array.notes)[0], getResources().getStringArray(R.array.notesBody)[0], getResources().getStringArray(R.array.notesDate)[0]);
         }
 
-        // Если можно нарисовать рядом герб, то сделаем это
+
         if (isLandscape) {
             showLandBody(currentNote);
         }
@@ -115,25 +110,19 @@ public class NotesFragment extends Fragment {
         }
     }
 
-    // Показать герб в ландшафтной ориентации
-    private void showLandBody(Note currentCity) {
-        // Создаем новый фрагмент с текущей позицией для вывода герба
-        NotesBodyFragment detail = NotesBodyFragment.newInstance(currentNote);
 
-        // Выполняем транзакцию по замене фрагмента
+    private void showLandBody(Note currentCity) {
+        NotesBodyFragment detail = NotesBodyFragment.newInstance(currentNote);
         FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.body, detail);  // замена фрагмента
+        fragmentTransaction.replace(R.id.body, detail);
         fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         fragmentTransaction.commit();
     }
 
-    // Показать герб в портретной ориентации.
     private void showPortBody(Note currentNote) {
-        // Откроем вторую activity
         Intent intent = new Intent();
         intent.setClass(getActivity(), NotesBodyActivity.class);
-        // и передадим туда параметры
         intent.putExtra(NotesBodyFragment.ARG_NOTE, currentNote);
         startActivity(intent);
     }
