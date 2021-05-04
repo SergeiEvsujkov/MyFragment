db15da0445bf0f106337cd1bab1387be24300778
coffeemolkin <evsujkov@yandex.ru>
2021-05-04 17:59:12 +0700

    Homework 7.final

--- app/src/main/java/com/example/myfragment/MainActivity.java
+++ app/src/main/java/com/example/myfragment/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.myfragment;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +26,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        initToolbar();
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
     }
 
-    private void initToolbar() {
+    // регистрация drawer
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Обработка навигационного меню
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (navigateFragment(id)){
+                    drawer.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private Toolbar initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        return toolbar;
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -34,19 +67,10 @@ public class MainActivity extends AppCompatActivity {
         // Обработка выбора пункта меню приложения (активити)
         int id = item.getItemId();
 
-        switch (id) {
-            case R.id.action_settings:
-                Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT)
-                        .show();
-                return true;
-            case R.id.action_main:
-                Toast.makeText(MainActivity.this, "Main", Toast.LENGTH_SHORT)
-                        .show();
-                return true;
-            case R.id.action_favorite:
-                Toast.makeText(MainActivity.this, "Favorite", Toast.LENGTH_SHORT)
-                        .show();
-                return true;
+
+
+        if (navigateFragment(id)) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -74,5 +98,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
         return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private boolean navigateFragment(int id){
+        switch (id) {
+            case R.id.action_settings:
+                Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT)
+                        .show();
+                return true;
+            case R.id.action_main:
+                Toast.makeText(MainActivity.this, "Main", Toast.LENGTH_SHORT)
+                        .show();
+                return true;
+            case R.id.action_favorite:
+                Toast.makeText(MainActivity.this, "Favorite", Toast.LENGTH_SHORT)
+                        .show();
+                return true;
+        } return false;
     }
 }
